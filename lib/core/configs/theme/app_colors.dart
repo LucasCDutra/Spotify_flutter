d630c823710552e8e59cb7566a374527fcfdde0a
LucasCDutra <dutra3751@gmail.com>
2025-01-18 10:52:38 -0300

    create app_colors

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0XFF42C83C);
+  static const lighBackground = Color(0xffF2F2F2);
+  static const darkBackground = Color(0xff0d0c0c);
+  static const grey = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xff343434);
+
+  // static const darkBackground = Color(0xff);
+}
